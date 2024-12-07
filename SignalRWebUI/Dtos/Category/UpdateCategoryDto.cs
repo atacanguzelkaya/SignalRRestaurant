5373063b4ef80fff70f4582f5d5e5827dbd9c5ec
atacanguzelkaya <atacangzlky@gmail.com>
2024-12-07 15:30:32 +0300

    Add AdminLayout and Category

---
+++ SignalRWebUI/Dtos/Category/UpdateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRWebUI.Dtos.Category
+{
+	public class UpdateCategoryDto
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public bool Status { get; set; }
+	}
+}
