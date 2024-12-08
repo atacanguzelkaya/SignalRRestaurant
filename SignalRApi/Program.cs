b50c369e7daee82a8819b6438a375efd7a85352b
atacanguzelkaya <atacangzlky@gmail.com>
2024-12-08 17:32:10 +0300

    Added SignalRHub

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,13 +1,24 @@
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.BusinessLayer.Container;
 using System.Reflection;
+using SignalRApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
+
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
 builder.Services.ContainerDependencies();
 
 builder.Services.AddControllers();
@@ -23,11 +34,14 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
